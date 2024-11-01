8f73c9e0f9a8ed7758b7d6b096594a1290e025a0
ASLawan <nawalnitsua@gmail.com>
2024-11-01 12:30:59 +0100

    Update - redo server task

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,26 +1,35 @@
-import redisClient from '../utils/redis.js';
-import dbClient from '../utils/db.js';
+import redisClient from "../utils/redis.js";
+import dbClient from "../utils/db.js";
 
 class AppController {
-	static async getStatus(req, res){
-		const redisAlive = redisClient.isAlive();
-		const dbAlive = dbClient.isAlive()
+  // GET /status
+  static async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
 
-		return res.status(200).json({redis: redisAlive, db: dbAlive});
-	}
+    res.status(200).json({
+      redis: redisStatus,
+      db: dbStatus,
+    });
+  }
 
-	static async getStats(req, res){
-		try{
-			const usersCount = await dbClient.nbUsers();
-			const filesCount = await dbClient.nbFiles();
+  // GET /stats
+  static async getStats(req, res) {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
 
-			return res.status(200).json({users: usersCount, files: filesCount})
-		} catch(error){
-			console.error('Error fetching stats:', error);
-			return res.status(500).json({error: 'Failed to retrieve stats'})
-		}
-	}
+      res.status(200).json({
+        users: usersCount,
+        files: filesCount,
+      });
+    } catch (error) {
+      console.error(`Error fetching stats: ${error.message}`);
+      res
+        .status(500)
+        .json({ error: "An error occurred while fetching stats." });
+    }
+  }
 }
 
 export default AppController;
-
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
-import {Router} from 'express';
-import AppController from '../controllers/AppController.js';
-import UsersController from '../controllers/UsersController.js';
-import AuthController from '../controllers/AuthController.js';
+import { Router } from "express";
+import AppController from "../controllers/AppController.js";
+// import UsersController from '../controllers/UsersController.js';
+// import AuthController from '../controllers/AuthController.js';
 
 const router = Router();
 
-router.get('/status', AppController.getStatus);
-router.get('./stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
+router.get("/status", AppController.getStatus);
+router.get("/stats", AppController.getStats);
+// router.post('/users', UsersController.postNew);
 
-router.get('/connect', AuthController.getConnect);
-router.get('./disconnect', AuthController.getDisconnect);
+// router.get('/connect', AuthController.getConnect);
+// router.get('./disconnect', AuthController.getDisconnect);
 
-router.get('/users/me', UsersController.getMe);
+// router.get('/users/me', UsersController.getMe);
 
-router.post('/files', FilesController.postUpload);
+// router.post('/files', FilesController.postUpload);
 
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
+// router.put('/files/:id/publish', FilesController.putPublish);
+// router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 export default router;
