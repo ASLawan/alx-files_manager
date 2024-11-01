66453e820c7ff5aa822c0bc706720161fd0ab681
ASLawan <nawalnitsua@gmail.com>
2024-11-01 14:47:25 +0100

    Redo create user task

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-import { Router } from "express";
-import AppController from "../controllers/AppController.js";
-// import UsersController from '../controllers/UsersController.js';
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 // import AuthController from '../controllers/AuthController.js';
 
 const router = Router();
 
-router.get("/status", AppController.getStatus);
-router.get("/stats", AppController.getStats);
-// router.post('/users', UsersController.postNew);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 // router.get('/connect', AuthController.getConnect);
 // router.get('./disconnect', AuthController.getDisconnect);
