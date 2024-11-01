7a45cc21dfa72f364028662fcc4c6d5f265298ae
ASLawan <nawalnitsua@gmail.com>
2024-11-01 22:02:04 +0100

    Update -routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -13,10 +14,9 @@ router.get('/connect', AuthController.getConnect);
 router.get('./disconnect', AuthController.getDisconnect);
 
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
 
-// router.post('/files', FilesController.postUpload);
-
-// router.put('/files/:id/publish', FilesController.putPublish);
-// router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 export default router;
